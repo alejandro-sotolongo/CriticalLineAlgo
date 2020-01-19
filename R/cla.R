acdd55e0492a65e29cba74766b8605227f7ec4ff
Alejandro <als1012@gmail.com>
2020-01-18 20:12:09 -0500

    added intro vign

--- R/cla.R
+++ R/cla.R
@@ -1,6 +1,106 @@
+#' @title Run the critical line algo
+#' @param mu_vec vector or column matrix of expected returns
+#' @param cov_mat expected covariance matrix
+#' @param low_bound lower bound weights, see details
+#' @param up_bound upper bound weights, see details
+#' @param max_iter maximum number of iterations to run while searching for corner
+#' portfolios
+#' @return a list containing the weights for each corner portfolio \code{wgt_list}
+#' @details The \code{low_bound} and \code{up_bound} weights are the corresponding 
+#' upper and lower bound weights for each asset in the \code{mu_vec}. They can
+#' be entered as a single numeric value if all assets have the same upper or lower
+#' bound weight or a vector of upper and lower bound weights can be entered.
+#' @export
+run_cla <- function(mu_vec, cov_mat, low_bound = 0, up_bound = 1,
+                    max_iter = 1000) {
+  
+  mu_vec <- matrix(mu_vec, ncol = 1)
+  store <- init_cla(mu_vec, low_bound, up_bound)
+  n_assets <- nrow(mu_vec)
+  store$l <- NA
+  store$wgt_list <- list(store$wgt_vec)
+  
+  for (iter in 1:max_iter) {
+    # Case a) bound a free weight
+    l_in <- -100
+    if (length(store$i_free) > 1) {
+      s <- sub_mat(mu_vec, cov_mat, store$wgt_vec, store$i_free)
+      j <- 1
+      for (i in store$i_free) {
+        l <- calc_lambda(s$cov_f_inv, s$cov_fb, s$mu_f, s$wgt_b, j,
+                         c(store$low_bound[i], store$up_bound[i]))
+        if (l$l_i > l_in) {
+          l_in <- l$l_i
+          i_in <- i
+          b_i_in <- l$b_i
+        }
+        j <- j + 1
+      }
+    }
+    # Case b) free an unbounded weight
+    l_out <- -100
+    if (length(store$i_free) < n_assets) {
+      b <- get_b(store$i_free, n_assets)
+      l_vec <- rep(0, length(b))
+      for (i in b) {
+        s <- sub_mat(mu_vec, cov_mat, store$wgt_vec, c(store$i_free, i))
+        l <- calc_lambda(s$cov_f_inv, s$cov_fb, s$mu_f, s$wgt_b, length(s$mu_f),
+                         store$wgt_vec[i])
+        old_l <- store$l[length(store$l)]
+        if ((is.na(old_l) | l$l_i < old_l) & l$l_i > l_out) {
+          l_out <- l$l_i
+          i_out <- i
+        }
+      }
+    }
+    # choose lambda
+    if (max(c(l_in, 0), na.rm = TRUE) > l_out) {
+      store$l <- c(store$l, l_in)
+      store$i_free <- store$i_free[!i_in]
+      store$wgt_vec[i_in] <- b_i_in
+    } else {
+      store$l <- c(store$l, l_out)
+      store$i_free <- c(store$i_free, i_out)
+    }
+    # if all weights are bound we have hit the lowest corner portfolio and can
+    # exit
+    if (length(store$i_free) == 0) {
+      break
+    }
+    # solve free weights and place into the stored weight list
+    s <- sub_mat(mu_vec, cov_mat, store$wgt_vec, store$i_free)
+    store$s <- s
+    wgt_f <- calc_wgt_f(s$cov_f_inv, s$cov_fb, s$mu_f, s$wgt_b,
+                        store$l[length(store$l)])
+    store$wgt_vec[store$i_free] <- wgt_f
+    store$wgt_list[[length(store$wgt_list) + 1]] <- store$wgt_vec
+    # if lambda hits zero we are at the last corner portfolio and can exit
+    if (l_out <= 0 & l_in <= 0) {
+      break
+    }
+  }
+  # calculate global min variance portfolio and add to store before returning
+  # stored values
+  s <- store$s
+  wgt_f <- calc_wgt_f(s$cov_f_inv, s$cov_fb, rep(0, length(s$mu_f)), s$wgt_b,
+                      store$l[length(store$l)])
+  store$wgt_vec[store$i_free] <- wgt_f
+  store$wgt_list[[length(store$wgt_list) + 1]] <- store$wgt_vec
+  return(store)
+}
 
-init_cla <- function(mu_vec, up_bound = 1, low_bound = 0) {
 
+init_cla <- function(mu_vec, low_bound, up_bound) {
+  
+  # param ----
+  # mu_vec = vector of expected returns
+  # low_bound = lower bound weights
+  # up_bound = upper bound weights
+  # ----
+  # To initialize the algo, we first find the portfolio highest return given the
+  # mu_vec and lower and upper bound constraints. The mu_vec and bounds are 
+  # structured as column matrixes and returned in the res list. The first free
+  # weight index is returned in the res list as i_free.
   n_assets <- nrow(mu_vec)
   up_bound_vec <- matrix(up_bound, ncol = 1, nrow = n_assets)
   low_bound_vec <- matrix(low_bound, ncol = 1, nrow = n_assets)
@@ -34,13 +134,19 @@ init_cla <- function(mu_vec, up_bound = 1, low_bound = 0) {
   return(res)
 }
 
+
 get_b <- function(f, n_assets) {
+  
+  # utility function to return bound weights given the free weight index and
+  # number of assets
   b <- 1:n_assets
   b[!b %in% f]
 }
 
-sub_mat <- function(mu_vec, cov_mat, wgt_vec, f) {
 
+sub_mat <- function(mu_vec, cov_mat, wgt_vec, f) {
+  
+  # subset matrixes based on the free weight index f
   res <- list()
   b <- get_b(f, nrow(mu_vec))
   res$cov_f <- cov_mat[f, f]
@@ -51,8 +157,10 @@ sub_mat <- function(mu_vec, cov_mat, wgt_vec, f) {
   return(res)
 }
 
+
 calc_lambda <- function(cov_f_inv, cov_fb, mu_f, wgt_b, i, b_i) {
 
+  # calculate lambdas
   one_f <- matrix(1, nrow = length(mu_f), ncol = 1)
   one_b <- matrix(1, nrow = length(wgt_b), ncol = 1)
   c1 <- t(one_f) %*% cov_f_inv %*% one_f
@@ -76,8 +184,10 @@ calc_lambda <- function(cov_f_inv, cov_fb, mu_f, wgt_b, i, b_i) {
   return(res)
 }
 
+
 calc_wgt_f <- function(cov_f_inv, cov_fb, mu_f, wgt_b, l) {
 
+  # calculate the free weight values
   one_f <- matrix(1, nrow = length(mu_f), ncol = 1)
   one_b <- matrix(1, nrow = length(wgt_b), ncol = 1)
   g1 <- t(one_f) %*% cov_f_inv %*% mu_f
@@ -92,71 +202,16 @@ calc_wgt_f <- function(cov_f_inv, cov_fb, mu_f, wgt_b, l) {
   return(wgt_f)
 }
 
-run_cla <- function() {
 
-  # delete later
-  data(ETF)
-  mu_vec <- colMeans(ret[, 2:ncol(ret)])
-  mu_vec <- matrix(mu_vec, ncol = 1)
-  cov_mat <- cov(ret[, 2:ncol(ret)])
-  # ----------
-  store <- init_cla(mu_vec)
-  n_assets <- nrow(mu_vec)
-  store$l <- NA
-  store$wgt_list <- list(store$wgt_vec)
-
-  for (iter in 1:1000) {
-    # Case a)
-    l_in <- NA
-    if (length(store$i_free) > 1) {
-      s <- sub_mat(mu_vec, cov_mat, store$wgt_vec, store$i_free)
-      j <- 1
-      for (i in store$i_free) {
-        l <- calc_lambda(s$cov_f_inv, s$cov_fb, s$mu_f, s$wgt_b, j,
-                         c(store$low_bound[i], store$up_bound[i]))
-        if (min(c(0, l$l_i), na.rm = TRUE) > min(c(0, l_in), na.rm = TRUE)) {
-          l_in <- l$l_i
-          i_in <- i
-          b_i_in <- l$b_i
-        }
-      }
-    }
-    # Case b)
-    l_out <- NA
-    if (length(store$i_free) < n_assets) {
-      b <- get_b(store$i_free, n_assets)
-      l_vec <- rep(0, length(b))
-      j <- 1
-      for (i in b) {
-        s <- sub_mat(mu_vec, cov_mat, store$wgt_vec, c(store$i_free, i))
-        l <- calc_lambda(s$cov_f_inv, s$cov_fb, s$mu_f, s$wgt_b, length(s$mu_f),
-                         store$wgt_vec[i])
-        j <- j + 1
-        if ((is.na(store$l) | l$l_i < store$l) & (l$l_i > 0 | l$l_i > l_out)) {
-          l_out <- l$l_i
-          i_out <- i
-        }
-      }
-    }
-
-    if ((is.na(l_out) | l_out == 0) & (is.na(l_in) | l_in == 0)) {
-      break
-    }
-
-    # decide lambda
-    if (max(c(l_in, 0), na.rm = TRUE) > l_out) {
-      store$l <- c(store$l, l_in)
-      store$i_free <- store$i_free[!i_in]
-      store$wgt_vec[i_in] <- b_i_in
-    } else {
-      store$l <- c(store$l, l_out)
-      store$i_free <- c(store$i_free, i_out)
-    }
-    s <- sub_mat(mu_vec, cov_mat, store$wgt_vec, store$i_free)
-    wgt_f <- calc_wgt_f(s$cov_f_inv, s$cov_fb, s$mu_f, s$wgt_b,
-                        store$l[length(store$l)])
-    store$wgt_vec[store$i_free] <- wgt_f
-    store$wgt_list[[length(store$wgt_list) + 1]] <- store$wgt_vec
-  }
+port_mu <- function(mu, w) {
+  # portfolio return
+  t(w) %*% mu
 }
+
+
+port_vol <- function(cov_mat, w) {
+  # portfolio standard deviation
+  sqrt(t(w) %*% cov_mat %*% w)
+}
+
 
